a9f9f23b003aed5210d85434441864e28d757520
Guzichenko Artem <aoguzich@gmail.com>
2017-11-23 00:53:31 +0200

    Program update

--- Core/src/main/java/com/guzichenko/courses/program/Main.java
+++ Core/src/main/java/com/guzichenko/courses/program/Main.java
@@ -4,8 +4,10 @@ import com.guzichenko.courses.program.services.CmdLineService;
 import com.guzichenko.courses.program.services.impl.ClientServiceImpl;
 import com.guzichenko.courses.program.services.impl.ProductServiceImpl;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		CmdLineService cmdLineService = new CmdLineService(new ClientServiceImpl(), new ProductServiceImpl());
 		cmdLineService.mainMenu();
--- Core/src/main/java/com/guzichenko/courses/program/services/CmdLineService.java
+++ Core/src/main/java/com/guzichenko/courses/program/services/CmdLineService.java
@@ -1,112 +1,114 @@
 package com.guzichenko.courses.program.services;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class CmdLineService {
 
-	private final Scanner scanner;
-	private final ClientService clientService;
-	private final ProductService productService;
+    private final BufferedReader reader;
+    private final ClientService clientService;
+    private final ProductService productService;
 
-	public CmdLineService(ClientService clientService, ProductService productService) {
-		this.productService = productService;
-		this.scanner = new Scanner(System.in);
-		this.clientService = clientService;
-	}
+    public CmdLineService(ClientService clientService, ProductService productService) {
+        this.productService = productService;
+        this.reader = new BufferedReader(new InputStreamReader(System.in));
+        this.clientService = clientService;
+    }
 
-	public void mainMenu() {
-		boolean isWork = true;
-		do {
-			showMainMenu();
-			int s = scanner.nextInt();
-			switch (s) {
-				case 1:
-					clientMenu();
-					break;
-				case 2:
-					productMenu();
-					break;
-				case 0:
-					isWork = false;
-					break;
-				default:
-					System.out.println("Неправильный ввод \n");
-					break;
-			}
-		}
-		while (isWork);
-	}
+    public void mainMenu() throws IOException {
+        boolean isWork = true;
+        do {
+            showMainMenu();
+            String s = reader.readLine();
+            switch (s) {
+                case "1":
+                    clientMenu();
+                    break;
+                case "2":
+                    productMenu();
+                    break;
+                case "0":
+                    isWork = false;
+                    break;
+                default:
+                    System.out.println("Неправильный ввод \n");
+                    break;
+            }
+        }
+        while (isWork);
+    }
 
-	private void clientMenu() {
-		boolean isWork = true;
-		do {
-			showClientMenu();
-			int s = scanner.nextInt();
-			switch (s) {
-				case 1:
-					clientService.addClient();
-					break;
-				case 2:
-					clientService.editClient();
-					break;
-				case 3:
-					clientService.removeClient();
-					break;
-				case 0:
-					isWork = false;
-					break;
-				default:
-					System.out.println("Неправильный ввод \n");
-					break;
-			}
-		}
-		while (isWork);
-	}
+    private void clientMenu() throws IOException {
+        boolean isWork = true;
+        do {
+            showClientMenu();
+            String s = reader.readLine();
+            switch (s) {
+                case "1":
+                    clientService.addClient();
+                    break;
+                case "2":
+                    clientService.editClient();
+                    break;
+                case "3":
+                    clientService.removeClient();
+                    break;
+                case "0":
+                    isWork = false;
+                    break;
+                default:
+                    System.out.println("Неправильный ввод \n");
+                    break;
+            }
+        }
+        while (isWork);
+    }
 
-	private void productMenu() {
-		boolean isWork = true;
-		do {
-			showProductMenu();
-			int s = scanner.nextInt();
-			switch (s) {
-				case 1:
-					productService.addProduct();
-					break;
-				case 2:
-					productService.editProduct();
-					break;
-				case 3:
-					productService.removeProduct();
-					break;
-				case 0:
-					isWork = false;
-					break;
-				default:
-					System.out.println("Неправильный ввод \n");
-					break;
-			}
-		}
-		while (isWork);
-	}
+    private void productMenu() throws IOException {
+        boolean isWork = true;
+        do {
+            showProductMenu();
+            String s = reader.readLine();
+            switch (s) {
+                case "1":
+                    productService.addProduct();
+                    break;
+                case "2":
+                    productService.editProduct();
+                    break;
+                case "3":
+                    productService.removeProduct();
+                    break;
+                case "0":
+                    isWork = false;
+                    break;
+                default:
+                    System.out.println("Неправильный ввод \n");
+                    break;
+            }
+        }
+        while (isWork);
+    }
 
-	private static void showMainMenu() {
-		System.out.println("1. Управление клиентами");
-		System.out.println("2. Управление продуктами");
-		System.out.println("0. Выход");
-	}
+    private static void showMainMenu() {
+        System.out.println("1. Управление клиентами");
+        System.out.println("2. Управление продуктами");
+        System.out.println("0. Выход");
+    }
 
-	private static void showProductMenu() {
-		System.out.println("1. Создать продукт");
-		System.out.println("2. Изменить продукт");
-		System.out.println("3. Удалить продукт");
-		System.out.println("0. Назад");
-	}
+    private static void showProductMenu() {
+        System.out.println("1. Создать продукт");
+        System.out.println("2. Изменить продукт");
+        System.out.println("3. Удалить продукт");
+        System.out.println("0. Назад");
+    }
 
-	private static void showClientMenu() {
-		System.out.println("1. Создать клиента");
-		System.out.println("2. Изменить клиента");
-		System.out.println("3. Удалить клиента");
-		System.out.println("0. Назад");
-	}
+    private static void showClientMenu() {
+        System.out.println("1. Создать клиента");
+        System.out.println("2. Изменить клиента");
+        System.out.println("3. Удалить клиента");
+        System.out.println("0. Назад");
+    }
 
 }
